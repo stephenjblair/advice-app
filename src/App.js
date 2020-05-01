b0591da87149d35429343415d6f96409633e714e
Stephen Blair <stephenjohnblair@outlook.com>
2020-05-01 12:58:39 +0100

    Created axios get request to api and setState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import './App.css'
+import axios from 'axios';
 
 class App extends React.Component {
+    state = { advice: "" };
+
+    componentDidMount() {
+        this.fetchAdvice();
+    }
+
+    fetchAdvice = () => {
+        axios.get('https://api.adviceslip.com/advice')
+        .then((response) => {
+            const { advice } = response.data.slip;
+            
+            this.setState({ advice });
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
     render () {
+        const { advice } = this.state
         return (
-            <h1>test</h1>
+            <div className="app">
+                <div className="card">
+                    <h1 className="heading">{advice}</h1>
+                </div>
+            </div>       
         )
     }
 }
